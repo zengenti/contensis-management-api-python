4e664502a1bdbc04898f258fb6d7c2f068eb9142
Joe J Collins <j.collins@zengenti.com>
2024-11-16 16:47:11 +0000

    Add python version

--- src/contensis_management/api_client.py
+++ src/contensis_management/api_client.py
@@ -10,26 +10,29 @@ LOGGER = logging.getLogger(__name__)
 class ApiClient:
     """A class to authenticate with the Contensis API."""
 
-    BASE_URL = "https://cms-develop.cloud.contensis.com"
-
     def __init__(
         self,
         the_handler: request_handler_abc.RequestHandlerABC,
+        alias: str,
         username: str,
         password: str,
     ):
         self.the_handler = the_handler
+        self.alias = alias
+        self.base_url = f"https://cms-{alias}.cloud.contensis.com"
         self.token = self.authenticate(username, password)
 
     def authenticate(self, username, password):
         """Authenticate with the Contensis API and return the token."""
-        url = f"{self.BASE_URL}/authenticate/connect/token"
+        url = f"{self.base_url}/authenticate/connect/token"
         headers = {
             "Content-Type": "application/x-www-form-urlencoded",
             "Accept": "application/json",
         }
         data = {
-            "scope": "openid offline_access Security_Administrator ContentType_Read ContentType_Write ContentType_Delete Entry_Read Entry_Write Entry_Delete Project_Read Project_Write Project_Delete Workflow_Administrator",
+            "scope": (
+                "openid offline_access Security_Administrator ContentType_Read ContentType_Write ContentType_Delete Entry_Read Entry_Write Entry_Delete Project_Read Project_Write Project_Delete Workflow_Administrator"
+            ),
             "grant_type": "contensis_classic",
             "username": username,
             "password": password,
--- tests/api_client_test.py
+++ tests/api_client_test.py
@@ -1,6 +1,11 @@
 """"""
 
-from contensis_management import api_client, api_response, request_handler_abc
+from contensis_management import (
+    api_client,
+    api_response,
+    request_handler,
+    request_handler_abc,
+)
 from tests.helper_config import env_config
 
 
@@ -13,19 +18,32 @@ class MockRequestHandler(request_handler_abc.RequestHandlerABC):
         )
 
     def get(self, url, headers=None):
-        return api_response.ApiResponse(
-            json_data={"token": "mocked-token"}, status_code=200
-        )
+        raise NotImplementedError("This should not have been called.")
 
 
-def test_api_client() -> None:
+def test_api_client_success() -> None:
     """Confirm that the class can authenticate."""
     # Arrange
-    handler = MockRequestHandler()
+    mock_request_handler = MockRequestHandler()
+    alias = "dummy-alias"
+    username = "dummy-username"
+    password = "dummy-password"
+    client = api_client.ApiClient(mock_request_handler, alias, username, password)
+    # # Act
+    token = client.token
+    # Assert
+    assert token is not None
+
+
+def test_api_client_failure() -> None:
+    """Confirm that the class can authenticate."""
+    # Arrange
+    mock_request_handler = request_handler.RequestHandler()
+    alias = "develop"
     username = env_config.username
     password = env_config.password
-    client = api_client.ApiClient(handler, username, password)
-    # # Act
+    client = api_client.ApiClient(mock_request_handler, alias, username, password)
+    # Act
     token = client.token
     # Assert
     assert token is not None
