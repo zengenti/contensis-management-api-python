67d95077122de7025aa99872295448a0aae85ce2
Joe J Collins <j.collins@zengenti.com>
2024-11-16 19:55:36 +0000

    Avoid circular imports

--- src/contensis_management/projects.py
+++ src/contensis_management/projects.py
@@ -1,11 +1,15 @@
-
 """Project methods for the Contensis Management API."""
 
-from contensis_management import api_client
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:  # to avoid circular imports.
+    from contensis_management import api_client
+
 
 class Projects:
     """A class to handle projects in the Contensis Management API."""
-    def __init__(self, the_api_client: api_client.ApiClient):
+
+    def __init__(self, the_api_client: "api_client.ApiClient"):
         self.client = the_api_client
 
     def list(self):
