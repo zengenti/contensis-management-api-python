f75ea4b759697d4b58143b30fac982698ab7b403
Joe J Collins <j.collins@zengenti.com>
2024-11-18 11:08:09 +0000

    Test get a single user

--- tests/resource_handlers/users_test.py
+++ tests/resource_handlers/users_test.py
@@ -21,19 +21,19 @@ mock_users_json: list[dict] = [
         "expiry": "2029-09-03T09:51:20.113",
         "failedLoginAttempts": 0,
         "failedLoginAttemptsSinceLastSuccess": 0,
-        "firstName": "",
+        "firstName": "Alice",
         "id": "209e4053-f71b-45a9-ba87-3f5595caaeee",
         "language": "en-GB",
         "lastFailedLogin": None,
         "lastLogin": None,
-        "lastName": "",
+        "lastName": "Wonderland",
         "modified": "2024-09-03T09:51:20.113Z",
         "optOutOfNotifications": False,
         "passwordChanged": "2024-09-03T09:51:20.113Z",
         "status": {"locked": False, "passwordResetRequired": False, "suspended": False},
         "successfulLoginAttempts": 0,
         "timezone": "Etc/UTC",
-        "username": "Alice",
+        "username": "user-alice",
     },
     {
         "created": "2024-09-02T12:11:49.46Z",
@@ -46,19 +46,19 @@ mock_users_json: list[dict] = [
         "expiry": "2029-09-02T12:11:49.46",
         "failedLoginAttempts": 0,
         "failedLoginAttemptsSinceLastSuccess": 0,
-        "firstName": "",
+        "firstName": "Bob",
         "id": "6ec2d6d8-0947-4322-ba08-5624a8206a00",
         "language": "en-GB",
         "lastFailedLogin": None,
         "lastLogin": None,
-        "lastName": "",
+        "lastName": "Builder",
         "modified": "2024-09-02T12:11:49.46Z",
         "optOutOfNotifications": False,
         "passwordChanged": "2024-09-02T12:11:49.46Z",
         "status": {"locked": False, "passwordResetRequired": False, "suspended": False},
         "successfulLoginAttempts": 0,
         "timezone": "Etc/UTC",
-        "username": "Bob",
+        "username": "user-bob",
     },
     {
         "created": "2024-07-15T13:47:45.373Z",
@@ -75,19 +75,19 @@ mock_users_json: list[dict] = [
         "expiry": None,
         "failedLoginAttempts": 0,
         "failedLoginAttemptsSinceLastSuccess": 0,
-        "firstName": "David",
+        "firstName": "Carol",
         "id": "7bdd3065-a4c3-4b61-8154-43ebc266f4ec",
         "language": "en-GB",
         "lastFailedLogin": None,
         "lastLogin": "2024-09-11T08:48:14.443Z",
-        "lastName": "Davids",
+        "lastName": "Christmas",
         "modified": "2024-07-15T13:47:45.373Z",
         "optOutOfNotifications": False,
         "passwordChanged": None,
         "status": {"locked": False, "passwordResetRequired": False, "suspended": False},
         "successfulLoginAttempts": 0,
         "timezone": "Europe/Paris",
-        "username": "Carol",
+        "username": "user-carol",
     },
 ]
 
@@ -100,17 +100,17 @@ mock_paged_users_json: dict = {
 }
 
 
-class MockRequestHandlerSuccessful(request_handler_abc.RequestHandlerABC):
+class MockRequestHandlerAllUsers(request_handler_abc.RequestHandlerABC):
     """A mock implementation of the RequestHandler class."""
 
     def post(self, url, headers=None, data=None):
-        """Return a dummy token that is a plausible length."""
+        """Return a dummy token when we authenticate."""
         return api_response.ApiResponse(
             status_code=200, json_data={"access_token": "mock_token"}
         )
 
     def get(self, url, headers=None):
-        """Return a list of projects."""
+        """Return a list of users."""
         return api_response.ApiResponse(
             json_data=mock_paged_users_json, status_code=http.HTTPStatus.OK
         )
@@ -119,7 +119,7 @@ class MockRequestHandlerSuccessful(request_handler_abc.RequestHandlerABC):
 def test_list_users() -> None:
     """Test the list users with a mock resource handler."""
     # Arrange
-    mock_request_handler = MockRequestHandlerSuccessful()
+    mock_request_handler = MockRequestHandlerAllUsers()
     client = api_client.ApiClient(mock_request_handler)
     users_handler = users.Users(client)
     # Act
@@ -127,3 +127,33 @@ def test_list_users() -> None:
     # Assert
     the_number_of_users = 3
     assert len(the_users) == the_number_of_users
+
+
+class MockRequestHandlerOneUser(request_handler_abc.RequestHandlerABC):
+    """A mock implementation of the RequestHandler class."""
+
+    def post(self, url, headers=None, data=None):
+        """Return a dummy token when we need to authenticate."""
+        return api_response.ApiResponse(
+            status_code=200, json_data={"access_token": "mock_token"}
+        )
+
+    def get(self, url, headers=None):
+        """Return a one user."""
+        return api_response.ApiResponse(
+            json_data=mock_users_json[0], status_code=http.HTTPStatus.OK
+        )
+
+
+def test_get_user() -> None:
+    """Test the get user with a mock resource handler."""
+    # Arrange
+    mock_request_handler = MockRequestHandlerOneUser()
+    client = api_client.ApiClient(mock_request_handler)
+    users_handler = users.Users(client)
+    # Act
+    the_user = users_handler.get(user_id="209e4053-f71b-45a9-ba87-3f5595caaeee")
+    # Assert
+    assert the_user.username == "user-alice"
+    assert the_user.first_name == "Alice"
+    assert the_user.last_name == "Wonderland"
